aee8a445dc7c180e446abe77adc1436ab0144794
Ryo Suzuki <ryosuzk@gmail.com>
2016-06-20 20:31:15 -0700

    Create express server for production

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-const webpack = require('webpack')
-const WebpackDevServer = require('webpack-dev-server')
-const config = require('./webpack.config')
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true
-}).listen(3000, '127.0.0.1', function (err) {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at localhost:3000');
-});
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack')
+  const WebpackDevServer = require('webpack-dev-server')
+  const config = require('./webpack.config')
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true
+  }).listen(3000, '127.0.0.1', function (err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Listening at localhost:3000');
+  });
+} else {
+  const path = require('path')
+  const express = require('express')
+  const port = (process.env.PORT || 3000)
+
+  const app = express()
+  const indexPath = path.join(__dirname, '/index.html')
+  const publicPath = express.static(path.join(__dirname))
+  app.use(express.static('./'))
+  app.get('/', function (_, res) {
+    res.sendFile(indexPath)
+  })
+  app.listen(port)
+  console.log(`Listening at http://localhost:${port}`)
+}
+
