f0b5961eb0b7e48f00aa3c3434ce79af666e1804
Ryo Suzuki <ryosuzk@gmail.com>
2016-06-21 11:51:17 -0700

    Mockup for data-1

--- src/index.js
+++ src/index.js
@@ -29,43 +29,56 @@ class App extends React.Component {
     super(props)
     this.state = {
       code: '',
-      steps: '',
-      step: 0
+      data_a: '',
+      data_b: '',
+      step_a: '',
+      step_b: '',
+      step: 0,
+      max: 0
     }
     this.next = this.next.bind(this);
     this.prev = this.prev.bind(this);
-    this.show = this.show.bind(this);
-    $.get(this.props.src, function (res) {
+    this.update = this.update.bind(this);
+    this.init = this.init.bind(this);
+    this.init()
+  }
+
+  init () {
+    $.get('sample/data-1.py', function (res) {
       this.setState({
-        code: res.code,
-        steps: res.steps
+        code: res,
       })
-      console.log(this)
+    }.bind(this))
+    $.get('sample/data-1.json', function (res) {
+      this.state.data_a = res.attempt
+      this.state.data_b = res.fixed
+      this.state.max = _.max([res.attempt.length, res.fixed.length])
+      this.setState(this.state)
     }.bind(this))
   }
 
-  show () {
-    let steps = this.state.steps
-    let current = _.slice(steps, 0, this.state.step)
-    this.state.current = current.join('\n')
+  update () {
+    this.state.step_a = _.slice(this.state.data_a, 0, this.state.step).join('\n')
+    this.state.step_b = _.slice(this.state.data_b, 0, this.state.step).join('\n')
     this.setState(this.state)
   }
 
   next () {
-    if (this.state.steps.length <= this.state.step) return
+    if (this.state.max <= this.state.step) return
     this.state.step++
-    this.show()
+    this.update()
   }
 
   prev () {
     if (0 >= this.state.step) return
     this.state.step--
-    this.show()
+    this.update()
   }
 
   render () {
     return <div className="ui grid">
-      <div className="eight wide column">
+      <div className="six wide column">
+        <h1>Attempt</h1>
         <pre><PrismCode className="language-python">{this.state.code}</PrismCode></pre>
         <div className="ui buttons">
          <button className="ui button" onClick={this.prev}>Back</button>
@@ -74,14 +87,19 @@ class App extends React.Component {
         </div>
         <p>{this.state.step}</p>
       </div>
-      <div className="eight wide column">
-        <pre><code prism className="language-python">{this.state.current}</code></pre>
+      <div className="four wide column">
+        <h1>Incorrect</h1>
+        <pre><code prism className="language-python">{this.state.step_a}</code></pre>
+      </div>
+      <div className="four wide column">
+        <h1>Correct</h1>
+        <pre><code prism className="language-python">{this.state.step_b}</code></pre>
       </div>
     </div>
   }
 }
 
-render(<App src='/sample.json' />, document.getElementById('root'))
+render(<App />, document.getElementById('root'))
 
 
 
