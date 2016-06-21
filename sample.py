58ba1f52a20bb4cb19feacf94b9ef151db178da6
Ryo Suzuki <ryosuzk@gmail.com>
2016-06-20 20:06:01 -0700

    Create mockup

--- sample.py
+++
@@ -1,7 +0,0 @@
-def product(n, term):
-  i = 1
-  product = 1
-  while i <= n:
-    product *= term(i)
-    i += 1
-  return product
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
+import _ from 'lodash'
 
 const codeStore = (state, action) => {
   switch (action.typ) {
@@ -25,16 +26,63 @@ const addCode = (content) => {
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { code: '' }
+    this.state = {
+      code: '',
+      steps: '',
+      step: 0
+    }
+    this.next = this.next.bind(this);
+    this.prev = this.prev.bind(this);
+    this.show = this.show.bind(this);
     $.get(this.props.src, function (res) {
-      this.setState({ code: res })
+      this.setState({
+        code: res.code,
+        steps: res.steps
+      })
       console.log(this)
     }.bind(this))
   }
 
+  show () {
+    let steps = this.state.steps
+    let current = _.slice(steps, 0, this.state.step)
+    this.state.current = current.join('\n')
+    this.setState(this.state)
+  }
+
+  next () {
+    if (this.state.steps.length <= this.state.step) return
+    this.state.step++
+    this.show()
+  }
+
+  prev () {
+    if (0 >= this.state.step) return
+    this.state.step--
+    this.show()
+  }
+
   render () {
-    return <pre><code prism className='language-java'>{this.state.code}</code></pre>
+    return <div className="ui grid">
+      <div className="eight wide column">
+        <pre><code prism className="language-python">{this.state.code}</code></pre>
+        <div className="ui buttons">
+         <button className="ui button" onClick={this.prev}>Back</button>
+         <div className="or"></div>
+         <button className="ui positive button" onClick={this.next}>Next</button>
+        </div>
+        <p>{this.state.step}</p>
+      </div>
+      <div className="eight wide column">
+        <pre><code prism className="language-python">{this.state.current}</code></pre>
+      </div>
+    </div>
   }
 }
 
-render(<App src='/sample.py' />, document.getElementById('root'))
+render(<App src='/sample.json' />, document.getElementById('root'))
+
+
+
+
+
