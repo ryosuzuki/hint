2a3d3a1c506465566ec7f242d4af7d3cd97491f8
Ryo Suzuki <ryosuzk@gmail.com>
2016-07-11 23:39:46 -0700

    Fix data flow

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import _ from 'lodash'
+import { ApSlider, ApSliderStyle } from 'apeman-react-slider'
 import { PrismCode } from 'react-prism'
 
 class App extends React.Component {
@@ -14,10 +15,10 @@ class App extends React.Component {
       data_b: '',
       step_a: '',
       step_b: '',
-      line: '',
+      lines: '',
       step: 0,
       max: 0,
-      height: 15
+      height: 35
     }
     this.next = this.next.bind(this);
     this.prev = this.prev.bind(this);
@@ -32,22 +33,58 @@ class App extends React.Component {
       this.setState({
         code: res,
       })
+      let len = res.split('\n').length
+      for (let i=0; i<len; i++) {
+        $('#output').append(`<div id="line-${i+1}"></div>`)
+      }
+
     }.bind(this))
     $.get(`sample/data-${this.state.id}.json`, function (res) {
       console.log('res')
       this.state.step = 0
-      this.state.line = res.line
+      this.state.lines = res.lines
       this.state.data_a = res.attempt
       this.state.data_b = res.fixed
       this.state.max = _.max([res.attempt.length, res.fixed.length])
       this.setState(this.state)
     }.bind(this))
+
+    $(function () {
+      $('#slider').range({
+        min: 0,
+        max: 10,
+        start: 0,
+        onChange: function (value) {
+          this.state.step = value
+          this.update()
+        }
+      })
+    })
+
   }
 
-  update () {
-    this.state.height = 15 + 21 * (this.state.line[this.state.step-1] - 1)
-    this.state.step_a = _.slice(this.state.data_a, 0, this.state.step).join('\n')
-    this.state.step_b = _.slice(this.state.data_b, 0, this.state.step).join('\n')
+  update (value) {
+    if (value) this.state.step = value
+    let line = this.state.lines[this.state.step-1]
+    let height = 35 + 21 * (line - 1)
+    let outputs = this.state.data_a[this.state.step-1]
+    let html = outputs.map( (output) => {
+      let v = output.var
+      let d = output.data
+      return `<span class='var'>${v}</span> = <span>${d}</span>`
+    }).join(', ')
+    $(`#line-${line}`).html(html)
+    this.state.height = height
+
+    if (this.state.step >= 5) {
+      $('#error')
+      .text(`x ${4} o 36`)
+      .css({top: height-5 })
+      .show()
+    }
+
+    // this.state.step_a = `<span id=${this.state.step}>${hoge}</span>` // _.slice(this.state.data_a, 0, this.state.step).join('\n')
+    // this.state.step_b = _.slice(this.state.data_b, 0, this.state.step).join('\n')
     this.setState(this.state)
   }
 
@@ -75,19 +112,40 @@ class App extends React.Component {
 
   render () {
     return <div className="ui grid">
-      <div id="main" className="six wide column">
+      <div id="main" className="ten wide column">
         <h1>Prototype</h1>
-        <pre id="container">
-          <i id="tick" className="fa fa-arrow-right" style={{top: this.state.height}}></i>
-          <PrismCode className="language-python" data-line="1">{this.state.code}</PrismCode>
-          <div id="output">{this.state.step_a}</div>
-        </pre>
+
+        <ApSliderStyle highlightColor="green"/>
+        <ApSlider
+          initial={50}
+          min={0}
+          max={10}
+          onChange={(value, ui) => {
+            value = Math.floor(value)
+            this.update(value)
+          }}
+        ></ApSlider>
         <div className="ui buttons">
         <button className="ui button" onClick={this.prev}>Back</button>
         <div className="or"></div>
         <button className="ui positive button" onClick={this.next}>Next</button>
         </div>
         <p>{this.state.step}</p>
+
+        <div className="ui grid">
+          <div id="container" className="twelve wide column">
+            <pre>
+              <i id="tick" className="fa fa-arrow-right" style={{top: this.state.height}}></i>
+              <PrismCode className="language-python" data-line="1">{this.state.code}</PrismCode>
+              <div id="output">{this.state.step_a}</div>
+            </pre>
+          </div>
+          <div id="outer" className="four wide column">
+            <div id="error" className="ui left pointing red basic label">
+              That name is taken!
+            </div>
+          </div>
+        </div>
         <button className="ui button" onClick={this.change}>Change</button>
       </div>
     </div>
@@ -95,10 +153,6 @@ class App extends React.Component {
 }
 
 render(<App />, document.getElementById('root'))
-
-
-
-
 
 const codeStore = (state, action) => {
   switch (action.typ) {
