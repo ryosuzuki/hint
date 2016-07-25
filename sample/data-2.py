85b1243075965fd78c9bcdf585ca74cb42ddfe94
Ryo Suzuki <ryosuzk@gmail.com>
2016-07-25 11:54:19 -0700

    Refactor with react + webpack

--- sample/data-2.py
+++
@@ -1,8 +0,0 @@
-def repeated(f, n):
-  def h(x):
-    i, total = 1, n
-    while i <= n:
-      total = f(total)
-      i += 1
-    return total
-  return h
--- src/bower.js
+++
@@ -1,6 +0,0 @@
-import 'jquery/dist/jquery.js'
-import 'semantic-ui/dist/semantic.js'
-import 'prism/prism.js'
-import 'prism/components/prism-python.js'
-import 'prism/plugins/highlight-keywords/prism-highlight-keywords.js'
-import 'prism/plugins/line-highlight/prism-line-highlight.js'
